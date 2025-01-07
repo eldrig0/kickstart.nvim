65680f6c0a9222db010714acc915b52315b75e82
rinzin <rinzin.wangchuk@quickbird.io>
2025-01-07 15:19:40 +0100

    add dart format and write

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,7 +11,31 @@ return {
       'stevearc/dressing.nvim', -- optional for vim.ui.select
     },
     config = function()
-      require('flutter-tools').setup {}
+      require('flutter-tools').setup {
+        lsp = {
+          on_attach = function(_, bufnr)
+            -- Enable format on save
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format {
+                  async = false,
+                  timeout_ms = 5000,
+                }
+              end,
+            })
+
+            -- Add format and save keybinding
+            vim.keymap.set('n', '<leader>fw', function()
+              vim.lsp.buf.format { async = false }
+              vim.cmd 'write'
+            end, { buffer = bufnr, desc = 'Format and save' })
+          end,
+          settings = {
+            lineLength = 80,
+          },
+        },
+      }
     end,
   },
 }
